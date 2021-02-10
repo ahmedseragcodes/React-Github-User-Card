e3ad35cfb6f3484bc15ab63c0ac73fe1fe579e53
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-09 20:22:31 -0500

    utilized react class component and componentwillmount to create github profile showcase

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -16,12 +16,33 @@ class App extends React.Component{
     }
   }
 
+  componentWillMount(){
+    axios.get(`${API}`)
+    .then((res)=>{
+      console.log(res.data);
+      this.setState({
+        username: res.data.login,
+        avatarURL: res.data.avatar_url,
+        bio: res.data.bio,
+        followers: res.data.followers,
+        following: res.data.following
+      })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
 
   render(){
     console.log(API);
     return (
       <div>
-
+        <h1>Github Profile Showcase</h1>
+        <h2>{this.state.username}</h2>
+        <img src={this.state.avatarURL} alt="github selfie" />
+        <p>Followers: {this.state.followers}</p>
+        <p>Following: {this.state.following}</p>
       </div>
     )
   }
